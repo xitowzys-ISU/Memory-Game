de1784ca0d93c1cbd6d160ea803b2cdd646b3c75
George Kalinka <xitowzys@gmail.com>
2021-12-08 13:44:30 +0800

    ✨ feat (MainActivity): output of playing cards with a size of 4 by 4

--- app/src/main/java/com/example/makelayout/MainActivity.kt
+++ app/src/main/java/com/example/makelayout/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.makelayout
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
@@ -9,18 +11,35 @@ import android.widget.LinearLayout
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val cat = ImageView(applicationContext)
-        cat.setImageResource(R.drawable.squarecat)
 
         val layout = LinearLayout(applicationContext)
         layout.orientation = LinearLayout.VERTICAL
-        layout.addView(cat)
 
-        val cat2 = ImageView(applicationContext)
-        cat2.setImageResource(R.drawable.squarecat)
-        layout.addView(cat2)
+        val params = LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT)
+        params.weight = 1.toFloat()
+
+        val catViews = ArrayList<ImageView>()
+        for (n in 1..16) {
+            catViews.add(ImageView(applicationContext).apply {
+                setImageResource(R.drawable.squarecat)
+                layoutParams = params
+            })
+        }
+
+        val rows = ArrayList<LinearLayout>();
+
+        for (i in 1..4) {
+            rows.add(LinearLayout(applicationContext).apply {
+                orientation = LinearLayout.HORIZONTAL
+                setBackgroundColor(Color.GRAY)
+                for (j in 1..4) addView(catViews[(i - 1) * 4 + (j - 1)])
+
+            }
+            )
+        }
+
+        for (row in rows) layout.addView(row)
 
         setContentView(layout)
-        //setContentView(R.layout.activity_main)
     }
 }
